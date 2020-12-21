a0865082e9a49ad1418d9d86014e2e37c0336ab8
Guillermo <guillermo1992x@gmail.com>
2020-12-21 13:58:32 +0100

    Arreglados movimientos a carpetas

--- resources/views/plantillas/plantilla_info.blade.php
+++ resources/views/plantillas/plantilla_info.blade.php
@@ -5,13 +5,10 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
 </head>
 <body>
-<<<<<<< HEAD:resources/views/plantilla_info.blade.php
-@include('nav')
-<section>
-=======
+
 @include('footer-nav.nav')
-<section class="">
->>>>>>> 4c9b2e8ca6aa45aa96f70950aa10b536a4c8ee43:resources/views/plantillas/plantilla_info.blade.php
+<section>
+
     <div class="row">
         <div class="col-3"></div>
         <div class="col-6 withBackground">
